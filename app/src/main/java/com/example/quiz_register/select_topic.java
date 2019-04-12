ed86bca8a776ed9a3e36ae442f4ca7dea7557691
ritik <ritikraushan777@gmail.com>
2019-04-12 20:32:29 +0530

    update: selection of topics works till here

--- app/src/main/java/com/example/quiz_register/select_topic.java
+++ app/src/main/java/com/example/quiz_register/select_topic.java
@@ -1,12 +1,20 @@
 package com.example.quiz_register;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class select_topic extends AppCompatActivity {
 
+    ImageButton imageButton2;
+    ArrayList listOfTopics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +23,20 @@ public class select_topic extends AppCompatActivity {
 
     public void clicked(View view){
         Intent intent = new Intent(select_topic.this,payment.class);
+
+        if(view.getTag().toString().equals("-1")) {
+            view.setSelected(true);
+            view.setTag("1");
+        }else
+        {
+            view.setSelected(false);
+            view.setTag("-1");
+        }
+    }
+
+
+    public void moveToPayment(View view){
+       Intent intent = new Intent(select_topic.this , payment.class);
         startActivity(intent);
     }
 }
